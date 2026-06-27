5a5d8f4b887430cf9d71063cb15eab691ca05de4
tungedng2710 <tungnguyen99.tn@gmail.com>
2026-06-28 00:53:18 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from "astro/config";
+import { unified } from "@astrojs/markdown-remark";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://tungedng2710.github.io",
   output: "static",
+  markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  },
 });
