18aa53451661847c0361c14bb214a31e1e750978
Tung Nguyen <tungedng2710@users.noreply.github.com>
2026-09-14 13:16:03 +0700

    update

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,19 +4,25 @@ export const projects = [
     image:
       "https://github.com/tungedng2710/AI-Traffic-Analysis/blob/main/data/assets/demo.jpg?raw=true",
     link: "https://github.com/tungedng2710/license-plate-recognition",
-    description: "Vietnamese license plate recognition",
+    description: "A comprehensive traffic surveillance and license plate recognition system",
   },
   {
     name: "DMOM Chatbot",
     image: "https://github.com/tungedng2710/DMOM-RAG/blob/main/assets/demo_dmom.png?raw=true",
     link: "https://github.com/tungedng2710/DMOM-RAG",
-    description: "Medical chatbot for dMOM Project",
+    description: "Medical chatbot for dMOM Project, sponsored by The Institute of Population, Health, and Development, Vietnam",
   },
   {
     name: "TonAI Imagine",
     image: "https://github.com/tungedng2710/TonAI-Imagine/blob/main/static/demo.png?raw=true",
     link: "https://github.com/tungedng2710/TonAI-Imagine",
     description: "A fast and efficient AI-powered image generation API",
+  },
+  {
+    name: "TonAI Data Studio",
+    image: "https://github.com/tungedng2710/TonAI-DataStudio/blob/main/assets/demo_datahub.png?raw=true",
+    link: "https://github.com/tungedng2710/TonAI-DataStudio",
+    description: "Mini datahub with data governance and data visualization",
   },
   {
     name: "TonAI OCR",
@@ -32,17 +38,11 @@ export const projects = [
     link: "https://github.com/tungedng2710/TonAI-Telegram-Bot",
     description: "Telegram bot with Ollama and ComfyUI",
   },
-  // {
-  //   name: "TonAI Creative",
-  //   image: "/assets/images/logo.png",
-  //   link: "https://github.com/tungedng2710/TonAI-Creative",
-  //   description: "A simple web UI for AI art creation",
-  // },
   {
     name: "Improved ArcFace",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ--nIIrBmO4iAtJjMoUXvtiL5_YZoEj8VwWQ&s",
     link: "https://github.com/tungedng2710/Improved-ArcFace",
-    description: "ArcFace model",
+    description: "Some improvements to the ArcFace model",
   },
 ] as const;
